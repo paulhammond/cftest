93ea220c7d2acde45c92a1de25ba524070f1ae61
Paul Hammond <paul@paulhammond.org>
2021-07-15 14:07:48 -0700

    Make output pretty

--- cmd/cftest/main.go
+++ cmd/cftest/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"strings"
-
-	"github.com/spf13/pflag"
-
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/spf13/pflag"
 
 	"github.com/paulhammond/cftest/internal/cftest"
 )
@@ -39,19 +39,44 @@ func main() {
 		os.Exit(1)
 	}
 
+	maxPathLen := 20
+	for _, t := range tests {
+		if len(t.Filename) > maxPathLen {
+			maxPathLen = len(t.Filename)
+		}
+	}
+	fmtPath := fmt.Sprintf("%%-%ds ", maxPathLen)
+
 	for _, t := range tests {
 		r, err := cftest.RunTest(runner, t)
+		path := fmt.Sprintf(fmtPath, t.Filename)
+		utilization := color.HiBlackString(fmt.Sprintf("(%d%%)", r.Utilization))
 
 		if err != nil {
-			fmt.Printf("ERROR: %s\n%s\n", t.Filename, err)
+			fmt.Printf("%s %s %s\n%s\n\n", color.RedString("✘"), path, color.RedString("ERROR"), err)
+			continue
 		}
 		if r.OK {
-			fmt.Printf("OK:    %s (%d)\n", t.Filename, r.Utilization)
+			fmt.Printf("%s %s %s    %s\n", color.GreenString("✔"), path, color.GreenString("ok"), utilization)
 		} else {
-			fmt.Printf("FAIL:  %s (%d)\n%s\n", t.Filename, r.Utilization, r.Failure)
+			fmt.Printf("%s %s %s  %s\n%s\n\n", color.RedString("✘"), path, color.RedString("FAIL"), utilization, colorDiff(r.Failure))
 		}
 
 	}
+}
+
+func colorDiff(diff string) string {
+	diff = strings.ReplaceAll(diff, "(-got +want)", fmt.Sprintf("(%s %s)", color.RedString("-got"), color.GreenString("+want")))
+	lines := strings.Split(diff, "\n")
+	for i, l := range lines {
+		if l[0] == '+' {
+			lines[i] = color.GreenString(l)
+		}
+		if l[0] == '-' {
+			lines[i] = color.RedString(l)
+		}
+	}
+	return strings.Join(lines, "\n")
 }
 
 func parseFunc(name string) (cftest.Runner, error) {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/aws/aws-sdk-go v1.39.1
+	github.com/fatih/color v1.12.0
 	github.com/kylelemons/godebug v1.1.0
 	github.com/spf13/pflag v1.0.5
 )
